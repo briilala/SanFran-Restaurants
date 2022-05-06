940d854431bfb517b61e63cacc80d0889314a687
briilala <brisan.brown@gmail.com>
2022-05-05 23:56:59 -0600

    environment setup

---
+++ src/main/java/src/test/java/com/sanfran/studentproject/StudentprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanfran.studentproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
